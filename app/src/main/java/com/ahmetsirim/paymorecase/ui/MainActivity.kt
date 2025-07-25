4e824a67ac393ca4a57c73adfa6f0adca147dc52
Ahmet SIRIM <ahmet.sirim@outlook.com>
2025-07-25 08:59:18 +0300

    Remove NavTracker logging from MainActivity
    This commit removes the navigation stack logging that was previously active in debug builds. The NavTracker logic, which logged the navigation route changes, has been removed from `MainActivity`.

--- app/src/main/java/com/ahmetsirim/paymorecase/ui/MainActivity.kt
+++ app/src/main/java/com/ahmetsirim/paymorecase/ui/MainActivity.kt
@@ -2,15 +2,12 @@ package com.ahmetsirim.paymorecase.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
-import com.ahmetsirim.paymorecase.BuildConfig
 import com.paymorecase.navigation.PaymoreCaseNavHost
 import com.paymorecase.ui.theme.PaymoreCaseTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,18 +21,7 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
-
-            // TODO: Remove the apply scope in the below
-            val navController = rememberNavController().apply {
-
-                if (!BuildConfig.DEBUG) return@apply
-
-                currentBackStack
-                    .collectAsStateWithLifecycle()
-                    .value
-                    .map { it.destination.route?.substringAfterLast('.') }
-                    .also { infoLog -> Log.i("InfoTag (NavTracker)", infoLog.toString()) }
-            }
+            val navController = rememberNavController()
 
             PaymoreCaseTheme {
                 PaymoreCaseNavHost(
