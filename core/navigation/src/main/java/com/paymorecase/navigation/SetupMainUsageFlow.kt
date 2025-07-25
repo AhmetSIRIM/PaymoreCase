fcc38d48cce60c55d05ba4d1522ca540ff5fdf33
Ahmet SIRIM <ahmet.sirim@outlook.com>
2025-07-25 08:58:20 +0300

    Implement slide and fade animations for navigation
    This commit introduces slide and fade animations for screen transitions in the application.
    
    The following navigation flows have been updated to include enter, exit, popEnter, and popExit transitions:
    - `SetupMainUsageFlow`
    - `SetupQrPaymentUsageFlow`
    - `SetupNfcPaymentUsageFlow`
    - `SetupSalesUsageFlow`
    
    Each transition uses a combination of `fadeIn`/`fadeOut` and `slideInHorizontally`/`slideOutHorizontally` with a `tween` animation spec of 300 milliseconds. This provides a smoother and more visually appealing user experience when navigating between screens.

--- core/navigation/src/main/java/com/paymorecase/navigation/SetupMainUsageFlow.kt
+++ core/navigation/src/main/java/com/paymorecase/navigation/SetupMainUsageFlow.kt
@@ -1,5 +1,10 @@
 package com.paymorecase.navigation
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -12,7 +17,32 @@ import com.paymorecase.sales.SalesRoute
 internal fun NavGraphBuilder.setupMainUsageFlow(
     navController: NavController,
 ) {
-    composable<MainRoute> {
+    composable<MainRoute>(
+        enterTransition = {
+            fadeIn(animationSpec = tween(300)) + slideInHorizontally(
+                initialOffsetX = { fullWidth -> fullWidth },
+                animationSpec = tween(300)
+            )
+        },
+        exitTransition = {
+            fadeOut(animationSpec = tween(300)) + slideOutHorizontally(
+                targetOffsetX = { fullWidth -> -fullWidth },
+                animationSpec = tween(300)
+            )
+        },
+        popEnterTransition = {
+            fadeIn(animationSpec = tween(300)) + slideInHorizontally(
+                initialOffsetX = { fullWidth -> -fullWidth },
+                animationSpec = tween(300)
+            )
+        },
+        popExitTransition = {
+            fadeOut(animationSpec = tween(300)) + slideOutHorizontally(
+                targetOffsetX = { fullWidth -> fullWidth },
+                animationSpec = tween(300)
+            )
+        }
+    ) {
         MainContainer(
             onNavigateToQR = { navController.navigate(QrPaymentRoute(paymentTypeEnum = it)) },
             onNavigateToNFC = { navController.navigate(NfcPaymentRoute(paymentTypeEnum = it)) },
